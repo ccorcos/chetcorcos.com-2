4b2e857bba38217727a36d0f225b593494c36609
Chet Corcos <ccorcos@gmail.com>
2017-10-08 21:51:42 -0700

    Better style

--- src/components/PostGallery.tsx
+++ src/components/PostGallery.tsx
@@ -3,6 +3,7 @@ import Link from "./Link"
 import Component from "reactive-magic/component"
 import * as postHelpers from "../helpers/postHelpers"
 import * as world from "../world"
+import * as _ from "lodash"
 
 export interface PostGalleryItemProps {
 	title: string
@@ -18,21 +19,33 @@ class PostGalleryItem extends Component<PostGalleryItemProps> {
 				href={postHelpers.getUrl(this.props)}
 				style={{
 					color: "#444",
-					width: 240,
 					margin: 12,
+					flex: 1,
 				}}
 			>
 				<div
 					style={{
-						width: 240,
-						height: 240,
-						backgroundImage: `url(${this.props.img})`,
-						backgroundSize: "cover",
-						backgroundPosition: "center",
-						borderRadius: 0,
-						marginBottom: 8,
+						position: "relative",
+						width: "100%",
+						paddingBottom: "100%",
+						height: 0,
 					}}
-				/>
+				>
+					<div
+						style={{
+							position: "absolute",
+							top: 0,
+							left: 0,
+							bottom: 0,
+							right: 0,
+							backgroundImage: `url(${this.props.img})`,
+							backgroundSize: "cover",
+							backgroundPosition: "center",
+							borderRadius: 0,
+							marginBottom: 8,
+						}}
+					/>
+				</div>
 				<div style={{ fontSize: 18 }}>{this.props.title}</div>
 				<div style={{ fontSize: 14, color: "gray" }}>{this.props.date}</div>
 			</Link>
@@ -41,19 +54,45 @@ class PostGalleryItem extends Component<PostGalleryItemProps> {
 }
 
 export default class PostGallery extends Component<{}> {
+	private getChunks() {
+		const { width } = world.windowSize.get()
+		if (width > 700) {
+			return 3
+		} else if (width > 400) {
+			return 2
+		} else {
+			return 1
+		}
+	}
+
+	private getRows() {
+		return _.chunk(world.posts.get(), this.getChunks())
+	}
+
 	view() {
+		const rows = this.getRows()
 		return (
 			<div
 				style={{
-					display: "inline-flex",
-					flexWrap: "wrap",
-					//justifyContent: "space-between",
+					display: "flex",
+					flexDirection: "column",
 					margin: -12,
 				}}
 			>
-				{world.posts
-					.get()
-					.map((post, index) => <PostGalleryItem key={index} {...post} />)}
+				{rows.map((row, i) => {
+					return (
+						<div key={i} style={{ display: "flex", flex: 1 }}>
+							{row.map((post, j) => <PostGalleryItem key={j} {...post} />)}
+							{Array(rows[0].length - row.length)
+								.fill(undefined)
+								.map((x, i) => {
+									return (
+										<div key={row.length + i} style={{ flex: 1, margin: 12 }} />
+									)
+								})}
+						</div>
+					)
+				})}
 			</div>
 		)
 	}
