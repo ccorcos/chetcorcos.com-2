54907d36151bee7bdd5e44ac11a037674ca7d9c9
Chet Corcos <ccorcos@gmail.com>
2017-11-22 04:42:37 -0800

    Postlist links

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -14,24 +14,30 @@ export interface PostListItemProps {
 class PostListItem extends Component<PostListItemProps> {
 	view() {
 		return (
-			<Link href={postHelpers.getUrl(this.props)} style={{ color: "#444" }}>
-				<div style={{ padding: 8, display: "flex" }}>
-					{this.props.img && (
-						<div
-							style={{
-								width: 65,
-								height: 65,
-								backgroundImage: `url(${this.props.img})`,
-								backgroundSize: "cover",
-								backgroundPosition: "center",
-								borderRadius: 6,
-							}}
-						/>
-					)}
-					<div style={{ flex: 1, paddingLeft: 8 }}>
-						<div style={{ fontSize: 18 }}>{this.props.title}</div>
-						<div style={{ fontSize: 14, color: "gray" }}>{this.props.date}</div>
-					</div>
+			<Link
+				href={postHelpers.getUrl(this.props)}
+				style={{
+					color: "#444",
+					padding: 8,
+					margin: "0px -8px",
+					display: "flex",
+				}}
+			>
+				{this.props.img && (
+					<div
+						style={{
+							width: 65,
+							height: 65,
+							backgroundImage: `url(${this.props.img})`,
+							backgroundSize: "cover",
+							backgroundPosition: "center",
+							borderRadius: 6,
+						}}
+					/>
+				)}
+				<div style={{ flex: 1, paddingLeft: 8 }}>
+					<div style={{ fontSize: 18 }}>{this.props.title}</div>
+					<div style={{ fontSize: 14, color: "gray" }}>{this.props.date}</div>
 				</div>
 			</Link>
 		)
