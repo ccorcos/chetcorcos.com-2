c826a1ff2a40a1b8ce885a1f7fa120264e7d84e0
Chet Corcos <ccorcos@gmail.com>
2017-11-26 14:07:23 -0800

    WIP

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,9 +1,10 @@
 import * as React from "react"
+import * as _ from "lodash"
 import Component from "reactive-magic/component"
 import Layout from "./Layout"
 import Footer from "./Footer"
-import Link from "./Link"
-import Markdown from "./Markdown"
+import NewsItem from "./NewsItem"
+import Subscribe from "./Subscribe"
 import { news } from "../newsData"
 
 export default class News extends Component<{}> {
@@ -26,69 +27,16 @@ export default class News extends Component<{}> {
 
 class NewsList extends Component<{}> {
 	view() {
+		const sorted = _.sortBy(news, item => {
+			return -1 * item.date
+		})
 		return (
 			<div>
-				{news.map(item => {
+				<Subscribe />
+				{sorted.map(item => {
 					return <NewsItem {...item} key={item.title} />
 				})}
 			</div>
 		)
-	}
-}
-
-export interface NewsItemProps {
-	title: string
-	date: number
-	url: string
-	description?: string
-}
-
-class NewsItem extends Component<NewsItemProps> {
-	view() {
-		return (
-			<Link
-				href={this.props.url}
-				style={{
-					color: "#444",
-					padding: 8,
-					margin: "0px -8px",
-					display: "flex",
-				}}
-			>
-				<div style={{ flex: 1, fontSize: 14 }}>
-					<div>
-						<span style={{ fontWeight: "bold" }}>{this.props.title}</span>
-						<span style={{ color: "gray" }}>
-							{" • "}
-							{this.renderDate()}
-						</span>
-					</div>
-					<div style={{ color: "gray" }}>
-						<Markdown text={this.props.description || ""} />
-					</div>
-				</div>
-			</Link>
-		)
-	}
-
-	private renderDate() {
-		const date = new Date(this.props.date)
-		const year = date.getFullYear()
-		const month = [
-			"January",
-			"February",
-			"March",
-			"April",
-			"May",
-			"June",
-			"July",
-			"August",
-			"September",
-			"October",
-			"November",
-			"December",
-		][date.getMonth()]
-		const day = date.getDate()
-		return `${month} ${day}, ${year}`
 	}
 }
